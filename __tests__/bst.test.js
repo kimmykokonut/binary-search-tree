acd1247ee0f039b3eefe12911923ea4598834905
Kim Robinson <kimmykokonut@gmail.com>
2024-03-04 14:48:33 -0800

    pass test for root node constructor

---
+++ __tests__/bst.test.js
@@ -0,0 +1,8 @@
+import BST from '../src/bst.js';
+
+describe('binarySearchTree', () => {
+  test('should initialize a binary search tree with a  root of null', () =>{
+    let bst = new BST();
+    expect(bst.root).toEqual(null);
+  })
+})
