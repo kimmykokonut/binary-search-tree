3d1558b9a969e38a84282f70abfdbe2112ee75e8
Kim Robinson <kimmykokonut@gmail.com>
2024-03-04 16:26:28 -0800

    all tests pass

--- __tests__/bst.test.js
+++ __tests__/bst.test.js
@@ -90,9 +90,9 @@ describe('binarySearchTree', () => {
     expect(pbst.search(4)).toEqual(true);
   });
 
-  // test('it should return false if the root node is not equal to 31', () => {
-  //   expect(pbst.search(31)).toEqual(false);
-  // });
+  test('it should return false if the root node is not equal to 31', () => {
+    expect(pbst.search(31)).toEqual(false);
+  });
 
   test('it should return true if the first child node on the left is equal to 2', () => {
     expect(pbst.search(2)).toEqual(true);
